38e39d4174b9b3d341380152fd56b4c4d3fb2ed6
Rodrigo <rorro@MacBook-Pro-de-Rodrigo-ZS.local>
2017-12-18 20:10:29 -0300

    heroku

--- My_Hue/main.py
+++ My_Hue/main.py
@@ -10,7 +10,7 @@ import hue_func as hue
 
 app = flask.Flask(__name__)
 
-@app.route('/api', methods = ['POST', 'GET', 'PATCH'])
+@app.route('/api', methods = ['POST', 'GET', 'PUT'])
 def api_get():
     req = flask.request.get_json()
 
@@ -24,7 +24,7 @@ def api_get():
     resto_mensaje = mensaje[ind_comando + 1 :]
 
     # comandos de Telegram
-    if comando == '/get':
+    if comando == '/info':
         light_num = resto_mensaje.strip(' ')
 
         if light_num.isdigit() or light_num == 'all':
@@ -50,90 +50,13 @@ def api_get():
             return "!"
 
     elif comando == '/post':
-        ind_num_issue = resto_mensaje.find(' ')
-        num_issue = resto_mensaje[: ind_num_issue]
-        respuesta = resto_mensaje[ind_num_issue + 1 :]
-
-        if num_issue.isdigit():
-            github.post_comment(num_issue, respuesta)
-            issue = github.get_issue(num_issue)
-
-            # Obtenemos datos
-            autor = issue['user']['login']
-            num_issue = issue['number']
-            titulo = issue['title']
-            link_issue = issue['url']
-
-            text = '''
-            [{0}] \n\n[#{1} - {2}] \n\nNuevo comentario: \n\n{3} \n\n[Link: {4}]
-
-            '''.format(autor, num_issue, titulo, respuesta, link_issue)
-
-            telegram.send_msg(chat_id, text)
-            return "!"
-
-        else:
-            text = 'El numero de la issue debe tener exclusivamente numeros'
-            telegram.send_msg(chat_id, text)
-            return "!"
+        pass
 
     elif comando == '/label':
-        ind_num_issue = resto_mensaje.find(' ')
-        num_issue = resto_mensaje[: ind_num_issue]
-        label = resto_mensaje[ind_num_issue + 1 :]
-
-        if num_issue.isdigit():
-            github.add_label(num_issue, label)
-            issue = github.get_issue(num_issue)
-
-            # Obtenemos datos
-            autor = issue['user']['login']
-            num_issue = issue['number']
-            titulo = issue['title']
-            link_issue = issue['url']
-
-            text = '''
-            [{0}] \n\n[#{1} - {2}] \n\nNueva Label: {3} \n\n[Link: {4}]
-
-            '''.format(autor, num_issue, titulo, label, link_issue)
-
-            telegram.send_msg(chat_id, text)
-            return "!"
-
-        else:
-            text = 'El numero de la issue debe tener exclusivamente numeros'
-            telegram.send_msg(chat_id, text)
-            return "!"
+        pass
 
     elif comando == '/close':
-        num_issue = resto_mensaje
-        print(num_issue)
-        print(type(num_issue))
-
-        if num_issue.isdigit():
-            issue = github.get_issue(num_issue)
-
-            # Obtenemos datos
-            autor = issue['user']['login']
-            num_issue = issue['number']
-            titulo = issue['title']
-            link_issue = issue['url']
-
-            text = '''
-            [{0}] \n\n[#{1} - {2}] \n\n- ISSUE CERRADA - \n\n[Link: {3}]
-
-            '''.format(autor, num_issue, titulo, link_issue)
-
-            # Cerramos
-            github.close_issue(num_issue)
-
-            telegram.send_msg(chat_id, text)
-            return "!"
-
-        else:
-            text = 'El numero de la issue debe tener exclusivamente numeros'
-            telegram.send_msg(chat_id, text)
-            return "!"
+        pass
 
     else:
         text = 'Lo siento, no te entendi bien'
